9a352f34e5591e707987088a2ac70c4f44a130de
Carlos <bwaycarl@gmail.com>
2020-12-01 23:06:50 -0500

    Working file - transitions in progress.

--- src/App.js
+++ src/App.js
@@ -1,34 +1,55 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import './App.css';
 import Navbar from "./components/Navbar"
-// import Wrapper from "./components/Wrapper";
-import Jumbotron from "./components/Jumbotron";
+import Wrapper from "./components/Wrapper";
+// import Jumbotron from "./components/Jumbotron";
+import Footer from "./components/Footer"
+
 import Home from "./pages/Home";
 import About from "./pages/About"
 import Resume from "./pages/Resume";
 import Contact from "./pages/Contact";
 import Portfolio from "./pages/Portfolio";
-import Footer from "./components/Footer"
 
-function App() {
-  return (
-    <div className="container">
-   <Router>
-      <Navbar />
-    
-        <Jumbotron>
-      <Route exact path="/" component={Home} />
-      <Route exact path="/about" component={About} />
-      <Route exact path="/contact" component={Contact} />
-      <Route exact path="/resume" component={Resume} />
-      <Route exact path="/portfolio" component={Portfolio} />
-      </Jumbotron>
-      <Footer />
-  
-  </Router>
-  </div>
-  );
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+// import {
+//   CSSTransition,
+//   TransitionGroup,
+// } from 'react-transition-group';
+
+class App extends Component {
+
+  render() {
+
+    return (
+      <div className="container">
+        <Wrapper>
+          <Router>
+            <Navbar />
+            {/* <Route render={({location}) => (
+              <TransitionGroup>
+                <CSSTransition
+                key={location.key}
+                  timeout={300}
+                  classNames="fade">
+                  <Switch location={location}> */}
+                    <Route exact path="/" component={Home} />
+                    <Route exact path="/about" component={About} />
+                    <Route exact path="/contact" component={Contact} />
+                    <Route exact path="/resume" component={Resume} />
+                    <Route exact path="/portfolio" component={Portfolio} />
+                  {/* </Switch>
+                </CSSTransition>
+              </TransitionGroup>
+            )} /> */}
+
+            <Footer />
+          </Router>
+        </Wrapper>
+      </div>
+    );
+  };
 }
-
 export default App;
