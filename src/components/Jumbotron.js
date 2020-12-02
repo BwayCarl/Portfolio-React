ba6d7026a8b2a82e0e2a02dec482a7cfe7db1c42
Carlos <bwaycarl@gmail.com>
2020-12-01 23:08:59 -0500

    In progress.

--- src/components/Jumbotron.js
+++ src/components/Jumbotron.js
@@ -1,12 +1,12 @@
-import React from "react";
-import "../styles/Jumbotron.css"
+// import React from "react";
+// import "../styles/Jumbotron.css"
 
-function Jumbotron(props) {
-  return (
-    <div className="jumbotron text-center" style={{ backgroundImage: `url(${props.backgroundImage})` }}>
-      {props.children}
-    </div>
-  );
-}
+// function Jumbotron() {
+//   return (
+//     <div className="jumbotron text-center" style={{ backgroundImage: `url(${props.backgroundImage})` }}>
+//       {props.children}
+//     </div>
+//   );
+// }
 
-export default Jumbotron;
+// export default Jumbotron;
