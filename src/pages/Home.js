bdca1d29a83b8bb462a8b2293a5c10405f7c5734
Carlos <bwaycarl@gmail.com>
2020-12-02 01:23:33 -0500

    Clean up.

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,16 +1,16 @@
 import React from 'react';
 import '../styles/Home.css';
 import { NavLink } from 'react-router-dom'
-// import Jumbotron from './components/Jumbotron.js';
+
+
 function Home() {
+
     return (
-        <div className="text-center bg-dark">
-            {/* <Jumbotron backgroundImage="../images/new-york-4340541_1920.jpg"> */}
+        <div className="jumbotron">
                 <h1 className="display-4">Hi, I'm Carlos.</h1>
-                <h2 className="">I'm a Full-Stack Web Developer student at Rutgers.</h2>
+                <h2 className="blurb">I'm a Full-Stack Web Developer student at Rutgers.</h2>
                 <br></br>
                 <NavLink className="btn btn-outline-secondary btn-lg text-center" to="/about">Learn more about me</NavLink>
-            {/* </Jumbotron> */}
         </div>
     )
 };
