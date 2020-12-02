bb82579cc58a4d5093fca5052878f895faa02945
Carlos <bwaycarl@gmail.com>
2020-12-01 23:05:57 -0500

    Adjustments for transitions - in progress.

--- src/index.js
+++ src/index.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter, Route } from 'react-router-dom';
 import './index.css';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <BrowserRouter>
+  <Route path="/" component={App} />
+   {/* <App />, */}
+ </BrowserRouter>,
   document.getElementById('root')
 );
 
