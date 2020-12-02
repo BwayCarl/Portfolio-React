2ccca750fb74ab315a0077b78a344532cb20d7a4
Carlos <bwaycarl@gmail.com>
2020-12-02 03:04:50 -0500

    Check.

--- src/index.js
+++ src/index.js
@@ -1,16 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter, Route } from 'react-router-dom';
+//import { BrowserRouter, Route } from 'react-router-dom';
 import './index.css';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
-  <BrowserRouter>
-  <Route path="/" component={App} />
-   {/* <App />, */}
- </BrowserRouter>,
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+//   <BrowserRouter>
+//   <Route path="/" component={App} />
+//    {/* <App />, */}
+//  </BrowserRouter>,
   document.getElementById('root')
 );
 
