1467e11ce770611abcfd8de650988529feeafbfc
Carlos <bwaycarl@gmail.com>
2020-12-19 17:02:15 -0500

    Refine descriptions.

--- src/pages/Portfolio.js
+++ src/pages/Portfolio.js
@@ -47,7 +47,7 @@ function Portfolio() {
                             <img src={process.env.PUBLIC_URL + "/assets/images/EMFLC.png"} className="card-img-top" alt="emflc-page" />
                             <div className="card-body text-white bg-dark">
                                 <h5 className="card-title">More Football Less COVID</h5>
-                                <p className="card-text">This team project leverages numerous technologies, throughout the full stack, to generate a user experience that gather data from multiple sources to provide tailored safety measures per NFL team in an attempt to mitigate COVID risk and hopefully allow for people to safely attend NFL games all season.</p>
+                                <p className="card-text">This team project leverages numerous technologies, throughout the full stack, to generate a user experience that gathers data from multiple sources to provide tailored safety measures per NFL team in an attempt to mitigate COVID risk and hopefully allow for people to safely attend NFL games all season.</p>
                                 <a href="https://fathomless-escarpment-94264.herokuapp.com/" rel="noreferrer" target="_blank" className="btn btn-secondary"><i class="fas fa-globe"></i> Visit the live page</a>
                                 &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
                                 <a href="https://github.com/BwayCarl/EvenMoreFootballLessCOVID" rel="noreferrer" target="_blank" className="btn btn-secondary"><i className="fab fa-github btn-icon"></i> Github Repository</a>
@@ -78,7 +78,7 @@ function Portfolio() {
                         <img src={process.env.PUBLIC_URL + "/assets/images/note-taker-app.png"} class="card-img-top" alt="note-taker-app" />
                                     <div class="card-body text-white bg-dark border-dark">
                                     <h5 class="card-title border-dark">Note Taker</h5>
-                                    <p class="card-text">An application that can be used to write, save, and delete notes using an express backend, saving and retrieving note data from a JSON file.</p>
+                                    <p class="card-text">An application that can be used to write, save, and delete notes using an express backend, saving and retrieving note data from a JSON file. Notes save as readonly until manually deleted.</p>
                                     <br/>
                                     <a href="https://afternoon-dawn-02847.herokuapp.com/" rel="noreferrer" target="_blank" class="btn btn-secondary"><i class="fas fa-globe"></i> Visit the live page</a>
                                     &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
@@ -92,7 +92,7 @@ function Portfolio() {
                             <img src={process.env.PUBLIC_URL + "/assets/images/Weather-Dashboard.png"} class="card-img-top" alt="weather-dashboard" />
                                 <div class="card-body text-white bg-dark">
                                     <h5 class="card-title">Weather Dashboard</h5>
-                                    <p class="card-text">A weather dashboard app using a third party API to return current weather information and a 5-day forecast for searched city.</p>
+                                    <p class="card-text">A weather dashboard app using a third party API to return current weather information and a 5-day forecast for a searched city, which remains in localstorage for future reference.</p>
                                     <br/>
                                     <a href="https://bwaycarl.github.io/weather-dashboard/" rel="noreferrer" target="_blank" class="btn btn-secondary"><i class="fas fa-globe"></i> Visit the live page</a>
                                     &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
