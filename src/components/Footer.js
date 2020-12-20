a2749f34c4b74d0ee6ca82099f34d0249ad3c2c9
Carlos <bwaycarl@gmail.com>
2020-12-20 13:10:52 -0500

    Change footer.

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,9 +4,9 @@ import "../styles/Footer.css"
 
 function Footer() {
   return (
-    <footer id="sticky-footer" className="py-3 mt-auto bg-dark fade-in-footer">
+    <footer id="sticky-footer" className="py-3 mt-auto bg-dark footer fade-in-footer">
                 <div className="footer-container-home text-right">
-                    <small> Copyright &copy; <a href="/contact">Carlos Martinez</a>&nbsp;&nbsp;&nbsp;</small>
+                    <small>  &copy; <a href="/contact">Carlos Martinez 2020</a>&nbsp;&nbsp;&nbsp;</small>
                 </div>
             </footer>
   );
