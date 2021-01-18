35afe65b90eecb3d503b0cbe9261e96e12bb7488
Carlos <bwaycarl@gmail.com>
2021-01-18 13:20:10 -0500

    Update resume.

--- src/pages/Resume.js
+++ src/pages/Resume.js
@@ -54,7 +54,7 @@ function Resume() {
                             {/* Summary */}
                         <h3>Summary</h3>
                                 <ol className="list-unstyled">
-                                    <li>After 25 successful years in the entertainment industry as a Broadway Local One stagehand specializing in electrics, the sudden coronavirus pandemic has steered me down a different path. While my career is on indefinite hold, I am taking the opportunity to expand my horizons and concentrate on learning the skills needed to jump start a second life in the world of web development.</li><br/>
+                                    <li>After 25 successful years in the entertainment industry as a Broadway Local One stagehand specializing in electrics, the sudden coronavirus pandemic has steered me down a different path. I am taking the opportunity to expand my horizons and concentrate on learning the skills needed to jump start a second life in the world of web development.</li><br/>
                                     <li>Having a strong work ethic and being in charge of large crews in conjunction with delegating responsibilities have honed my leadership skills and ingrained in me the importance of teamwork and appreciation of the vast skills needed to form a functioning team unit.</li>
                                 </ol>
                         </div>
@@ -72,7 +72,7 @@ function Resume() {
                                     <li>Responsible for all areas of lighting and electrical power for the entire theater, as well as the power needs for the lighting design, sound design and automation elements of the scenic design of each production.</li>
                                     <li>In charge of hiring the Electrics Department crew that loads in each attraction (numbers range from 12-30 crew members depending on the production) and the permanent crew that runs and maintains the show during its life.</li>
                                     <li>Implement initiatives for upgrading the facility's electrical systems to ensure up to date technology and safely replace outdated systems.</li>
-                                    <li>Submit monthly maintenance reports explaining work completed and recommendations for future projects.</li>
+                                    <li>Submit monthly maintenance reports with explanations of completed tasks and goals, including recommendations for future projects.</li>
                                 </ul>
                             </div>
                             <br/>
